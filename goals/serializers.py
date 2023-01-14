873720d833b1dbaf40c707c194b9f8ffea05a504
katerina-sys <13245545Zx>
2023-01-14 23:16:48 +0300

    lesson-36

---
+++ goals/serializers.py
@@ -0,0 +1,91 @@
+from django.db import transaction
+from rest_framework import serializers
+
+from core.models import User
+from core.serializers import UserSerializer
+from goals.models import GoalCategory, Goal, GoalComment
+
+
+class GoalCategoryCreateSerializer(serializers.ModelSerializer):
+    user = serializers.HiddenField(default=serializers.CurrentUserDefault())
+
+    class Meta:
+        model = GoalCategory
+        read_only_fields = ("id", "created", "updated", "user")
+        fields = "__all__"
+
+    def validate_category(self, value):
+        if value.is_deleted:
+            raise serializers.ValidationError("not allowed in deleted category")
+
+        if value.user != self.context["request"].user:
+            raise serializers.ValidationError("not owner of category")
+
+        return value
+
+
+class GoalCategorySerializer(serializers.ModelSerializer):
+    user = UserSerializer(read_only=True)
+
+    class Meta:
+        model = GoalCategory
+        fields = "__all__"
+        read_only_fields = ("id", "created", "updated", "user", 'board')
+
+    # def update(self, instance, validated_data):
+    #     board = validated_data.get('board')
+    #     if instance.board.id != board.id:
+    #         raise serializers.ValidationError("Cannot change board of category")
+    #     return super().update(instance, validated_data)
+
+
+class GoalCreateSerializer(serializers.ModelSerializer):
+    # category = serializers.PrimaryKeyRelatedField(queryset=GoalCategory.objects.all())
+    user = serializers.HiddenField(default=serializers.CurrentUserDefault())
+
+    class Meta:
+        model = Goal
+        read_only_fields = ("id", "created", "updated", "user")
+        fields = "__all__"
+
+    def validate_category(self, value):
+        if value.is_deleted:
+            raise serializers.ValidationError("not allowed in deleted category")
+
+        # if value.user != self.context["request"].user:
+        #     raise serializers.ValidationError("not owner of category")
+
+        return value
+
+
+class GoalSerializer(serializers.ModelSerializer):
+    user = UserSerializer(read_only=True)
+
+    class Meta:
+        model = Goal
+        fields = "__all__"
+        read_only_fields = ("id", "created", "updated", "user")
+
+    # def validate_category(self, value):
+    #     if value.user != self.context["request"].user:
+    #         raise serializers.ValidationError("not owner of category")
+    #
+    #     return value
+
+
+class GoalCommentCreateSerializer(serializers.ModelSerializer):
+    user = serializers.HiddenField(default=serializers.CurrentUserDefault())
+
+    class Meta:
+        model = GoalComment
+        read_only_fields = ("id", "created", "updated", "user")
+        fields = "__all__"
+
+
+class GoalCommentSerializer(serializers.ModelSerializer):
+    user = UserSerializer(read_only=True)
+
+    class Meta:
+        model = GoalComment
+        fields = "__all__"
+        read_only_fields = ("id", "created", "updated", "user")
--- todolist/urls.py
+++ todolist/urls.py
@@ -20,4 +20,5 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/core/", include('core.urls')),
     path("api/oauth/", include("social_django.urls", namespace="social")),
+    path("goals/", include("goals.urls")),
 ]
