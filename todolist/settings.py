5f9be52728861fa7231fd720641266b107b2f2fa
katerina-sys <13245545Zx>
2023-01-15 21:14:10 +0300

    lesson-36

--- todolist/settings.py
+++ todolist/settings.py
@@ -42,11 +42,11 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "goals",
-    "core",
     "rest_framework",
-    "corsheaders",
     "social_django",
+    "corsheaders",
+    "core",
+    "goals",
 ]
 
 MIDDLEWARE = [
