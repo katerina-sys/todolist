eb3efe50a1f2d873bf0a8f134624aa9f26cf4772
katerina-sys <13245545Zx>
2023-02-09 13:47:11 +0300

    lesson-38

--- bot/management/commands/runbot.py
+++ bot/management/commands/runbot.py
@@ -37,96 +37,95 @@ class Command(BaseCommand):
         self.tg_client = TgClient(settings.BOT_TOKEN)
         print('Bot have been started pooling...')
 
-    # TODO: НЕ знаю на сколько корректный данный код
-    # def choose_category(self, msg: Message, tg_user: TgUser):
-    #     goal_categories = GoalCategory.objects.filter(
-    #         board__participants__user=tg_user.user,
-    #         is_deleted=False,
-    #     )
-    #     goal_categories_str = '\n'.join(['- ' + goal.title for goal in goal_categories])
-    #
-    #     self.tg_client.send_message(
-    #         chat_id=msg.chat.id,
-    #         text=f"Выберите категорию: \n {goal_categories_str}"
-    #     )
-    #     STATE.set_state(TgState.CATEGORY_CHOOSE)
-    #
-    # def check_category(self, msg: Message):
-    #     category = GoalCategory.objects.filter(title=msg.text).first()
-    #     if category:
-    #         self.tg_client.send_message(
-    #             chat_id=msg.chat.id,
-    #             text=f"Введите заголовок цели"
-    #         )
-    #         STATE.set_category_id(category.id)
-    #         STATE.set_state(TgState.GOAL_CREATE)
-    #     else:
-    #         self.tg_client.send_message(
-    #             chat_id=msg.chat.id,
-    #             text=f"Категории '{msg.text}' не существует"
-    #         )
-    #
-    # def create_goal(self, msg: Message, tg_user: TgUser):
-    #     category = GoalCategory.objects.get(pk=STATE.category_id)
-    #     goal = Goal.objects.create(
-    #         title=msg.text,
-    #         user=tg_user.user,
-    #         category=category,
-    #         due_date=datetime.now().date(),
-    #     )
-    #     self.tg_client.send_message(
-    #         chat_id=msg.chat.id,
-    #         text=f'Цель "{goal.title}" создана!'
-    #     )
-    #     STATE.set_state(TgState.DEFAULT)
-    #
-    # def get_goals(self, msg: Message, tg_user: TgUser):
-    #     goals = Goal.objects.filter(
-    #         category__board__participants__user=tg_user.user,
-    #     ).exclude(status=Goal.Status.archived)
-    #     goals_str = '\n'.join([goal.title for goal in goals])
-    #
-    #     self.tg_client.send_message(
-    #         chat_id=msg.chat.id,
-    #         text=f'Вот список ваших целей:\n {goals_str}'
-    #     )
-    #
-    # def cancel_operation(self, msg: Message):
-    #     STATE.set_state(TgState.DEFAULT)
-    #     self.tg_client.send_message(
-    #         chat_id=msg.chat.id,
-    #         text=f"Операция отменена"
-    #     )
-    #
-    # def handle_message(self, msg: Message):
-    #     tg_user, created = TgUser.objects.get_or_create(
-    #         tg_user_id=msg.msg_from.id,
-    #         tg_chat_id=msg.chat.id,
-    #     )
-    #     if created:
-    #         tg_user.generate_validation_code()
-    #         self.tg_client.send_message(
-    #             chat_id=msg.chat.id,
-    #             text=f"Подтвердите, пожалуйста, свой аккаунт."
-    #                  f"Для подтверждения необходимо ввести код: {tg_user.verification_code} на сайте."
-    #         )
-    #
-    #     if msg.text == '/goals':
-    #         self.get_goals(msg, tg_user)
-    #     elif msg.text == '/create':
-    #         self.choose_category(msg, tg_user)
-    #     elif msg.text == '/cancel':
-    #         self.cancel_operation(msg)
-    #     elif STATE.state == TgState.CATEGORY_CHOOSE:
-    #         self.check_category(msg)
-    #     elif STATE.state == TgState.GOAL_CREATE:
-    #         self.create_goal(msg, tg_user)
-    #
-    #     else:
-    #         self.tg_client.send_message(
-    #             chat_id=msg.chat.id,
-    #             text=f"Неизвестная команда {msg.text}"
-    #         )
+    def choose_category(self, msg: Message, tg_user: TgUser):
+        goal_categories = GoalCategory.objects.filter(
+            board__participants__user=tg_user.user,
+            is_deleted=False,
+        )
+        goal_categories_str = '\n'.join(['- ' + goal.title for goal in goal_categories])
+
+        self.tg_client.send_message(
+            chat_id=msg.chat.id,
+            text=f"Выберите категорию: \n {goal_categories_str}"
+        )
+        STATE.set_state(TgState.CATEGORY_CHOOSE)
+
+    def check_category(self, msg: Message):
+        category = GoalCategory.objects.filter(title=msg.text).first()
+        if category:
+            self.tg_client.send_message(
+                chat_id=msg.chat.id,
+                text=f"Введите заголовок цели"
+            )
+            STATE.set_category_id(category.id)
+            STATE.set_state(TgState.GOAL_CREATE)
+        else:
+            self.tg_client.send_message(
+                chat_id=msg.chat.id,
+                text=f"Категории '{msg.text}' не существует"
+            )
+
+    def create_goal(self, msg: Message, tg_user: TgUser):
+        category = GoalCategory.objects.get(pk=STATE.category_id)
+        goal = Goal.objects.create(
+            title=msg.text,
+            user=tg_user.user,
+            category=category,
+            due_date=datetime.now().date(),
+        )
+        self.tg_client.send_message(
+            chat_id=msg.chat.id,
+            text=f'Цель "{goal.title}" создана!'
+        )
+        STATE.set_state(TgState.DEFAULT)
+
+    def get_goals(self, msg: Message, tg_user: TgUser):
+        goals = Goal.objects.filter(
+            category__board__participants__user=tg_user.user,
+        ).exclude(status=Goal.Status.archived)
+        goals_str = '\n'.join([goal.title for goal in goals])
+
+        self.tg_client.send_message(
+            chat_id=msg.chat.id,
+            text=f'Вот список ваших целей:\n {goals_str}'
+        )
+
+    def cancel_operation(self, msg: Message):
+        STATE.set_state(TgState.DEFAULT)
+        self.tg_client.send_message(
+            chat_id=msg.chat.id,
+            text=f"Операция отменена"
+        )
+
+    def handle_message(self, msg: Message):
+        tg_user, created = TgUser.objects.get_or_create(
+            tg_user_id=msg.msg_from.id,
+            tg_chat_id=msg.chat.id,
+        )
+        if created:
+            tg_user.generate_validation_code()
+            self.tg_client.send_message(
+                chat_id=msg.chat.id,
+                text=f"Подтвердите, пожалуйста, свой аккаунт."
+                     f"Для подтверждения необходимо ввести код: {tg_user.verification_code} на сайте."
+            )
+
+        if msg.text == '/goals':
+            self.get_goals(msg, tg_user)
+        elif msg.text == '/create':
+            self.choose_category(msg, tg_user)
+        elif msg.text == '/cancel':
+            self.cancel_operation(msg)
+        elif STATE.state == TgState.CATEGORY_CHOOSE:
+            self.check_category(msg)
+        elif STATE.state == TgState.GOAL_CREATE:
+            self.create_goal(msg, tg_user)
+
+        else:
+            self.tg_client.send_message(
+                chat_id=msg.chat.id,
+                text=f"Неизвестная команда {msg.text}"
+            )
 
     def handle(self, *args, **options):
         offset = 0
--- bot/views.py
+++ bot/views.py
@@ -1,23 +1,28 @@
-from rest_framework import permissions
-from rest_framework.generics import GenericAPIView
+from django.shortcuts import render
+
+#Create your views here.
+
+from rest_framework import generics, permissions, status
 from rest_framework.response import Response
 
 from bot.models import TgUser
+from bot.tg.client import TgClient
 from bot.tg.serializers import TgUserSerializer
 
 
-class BotVerifyView(GenericAPIView):
+class BotVerifyView(generics.UpdateAPIView):
     model = TgUser
     permission_classes = [permissions.IsAuthenticated]
+    http_method_names = ['patch']
     serializer_class = TgUserSerializer
 
     def patch(self, request, *args, **kwargs):
-        s: TgUserSerializer = self.get_serializer(data=request.data)
-        s.is_valid(raise_exception=True)
-
-        tg_user: TgUser = s.validated_data['tg_user']
-        tg_user.user = self.request.user
-        tg_user.save(update_fields=['user'])
-        instance_s: TgUserSerializer = self.get_serializer(tg_user)
-
-        return Response(instance_s.data)
+        data = self.serializer_class(request.data).data
+        tg_client = TgClient("6138310967:AAHJbueE7x5U6qW6C3vVZNFsLPC1yTbYujo")
+        tg_user = TgUser.objects.filter(verification_code=data['verification_code']).first()
+        if not tg_user:
+            return Response(status.HTTP_400_BAD_REQUEST)
+        tg_user.user = request.user
+        tg_user.save()
+        tg_client.send_message(chat_id=tg_user.tg_chat_id, text="Успешно")
+        return Response(data=data, status=status.HTTP_201_CREATED)
