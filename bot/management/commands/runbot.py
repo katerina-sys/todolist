cb4d41d61ac7ac5b8feac480797ef4d4c5138785
Катерина Акатовская <105228803+katerina-sys@users.noreply.github.com>
2023-02-09 13:34:37 +0300

    Update runbot.py

--- bot/management/commands/runbot.py
+++ bot/management/commands/runbot.py
@@ -1,16 +1,141 @@
-from aiogram.utils import executor
+from datetime import datetime
+
+from django.conf import settings
 from django.core.management import BaseCommand
 
-from bot.management.commands.create_bot import dp
-from bot.tg import client
-from bot.tg import dc
+from bot.models import TgUser
+from bot.tg.client import TgClient
+from bot.tg.dc import Message
+from goals.models import Goal, GoalCategory
+
+
+class TgState:
+    DEFAULT = 0
+    CATEGORY_CHOOSE = 1
+    GOAL_CREATE = 2
+
+    def __init__(self, state, category_id=None):
+        self.state = state
+        self.category_id = category_id
+
+    def set_state(self, state):
+        self.state = state
+
+    def set_category_id(self, category_id):
+        self.category_id = category_id
+
+
+STATE = TgState(state=TgState.DEFAULT)
 
 
 class Command(BaseCommand):
-    """Запуск бота через manage.py"""
-    help = 'run bot'
+    "запуск бота через manage.py"
+    help = 'Runs telegram bot'
 
-    def handle(self, *args, **kwargs):
-        dc.register_handlers_bot(dp)
-        client.register_handlers_client(dp)
-        executor.start_polling(dp, skip_updates=True)
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.tg_client = TgClient(settings.BOT_TOKEN)
+        print('Bot have been started pooling...')
+
+    # TODO: НЕ знаю на сколько корректный данный код
+     def choose_category(self, msg: Message, tg_user: TgUser):
+         goal_categories = GoalCategory.objects.filter(
+             board__participants__user=tg_user.user,
+             is_deleted=False,
+         )
+         goal_categories_str = '\n'.join(['- ' + goal.title for goal in goal_categories])
+    
+         self.tg_client.send_message(
+             chat_id=msg.chat.id,
+             text=f"Выберите категорию: \n {goal_categories_str}"
+         )
+         STATE.set_state(TgState.CATEGORY_CHOOSE)
+    
+     def check_category(self, msg: Message):
+         category = GoalCategory.objects.filter(title=msg.text).first()
+         if category:
+             self.tg_client.send_message(
+                 chat_id=msg.chat.id,
+                 text=f"Введите заголовок цели"
+             )
+             STATE.set_category_id(category.id)
+             STATE.set_state(TgState.GOAL_CREATE)
+         else:
+             self.tg_client.send_message(
+                 chat_id=msg.chat.id,
+                 text=f"Категории '{msg.text}' не существует"
+             )
+    
+     def create_goal(self, msg: Message, tg_user: TgUser):
+         category = GoalCategory.objects.get(pk=STATE.category_id)
+         goal = Goal.objects.create(
+             title=msg.text,
+             user=tg_user.user,
+             category=category,
+             due_date=datetime.now().date(),
+         )
+         self.tg_client.send_message(
+             chat_id=msg.chat.id,
+             text=f'Цель "{goal.title}" создана!'
+         )
+         STATE.set_state(TgState.DEFAULT)
+    
+     def get_goals(self, msg: Message, tg_user: TgUser):
+         goals = Goal.objects.filter(
+             category__board__participants__user=tg_user.user,
+         ).exclude(status=Goal.Status.archived)
+         goals_str = '\n'.join([goal.title for goal in goals])
+    
+         self.tg_client.send_message(
+             chat_id=msg.chat.id,
+             text=f'Вот список ваших целей:\n {goals_str}'
+         )
+    
+     def cancel_operation(self, msg: Message):
+         STATE.set_state(TgState.DEFAULT)
+         self.tg_client.send_message(
+             chat_id=msg.chat.id,
+             text=f"Операция отменена"
+         )
+    
+     def handle_message(self, msg: Message):
+         tg_user, created = TgUser.objects.get_or_create(
+             tg_user_id=msg.msg_from.id,
+             tg_chat_id=msg.chat.id,
+         )
+         if created:
+             tg_user.generate_validation_code()
+             self.tg_client.send_message(
+                 chat_id=msg.chat.id,
+                 text=f"Подтвердите, пожалуйста, свой аккаунт."
+                      f"Для подтверждения необходимо ввести код: {tg_user.verification_code} на сайте."
+             )
+    
+         if msg.text == '/goals':
+             self.get_goals(msg, tg_user)
+         elif msg.text == '/create':
+             self.choose_category(msg, tg_user)
+         elif msg.text == '/cancel':
+             self.cancel_operation(msg)
+         elif STATE.state == TgState.CATEGORY_CHOOSE:
+             self.check_category(msg)
+         elif STATE.state == TgState.GOAL_CREATE:
+             self.create_goal(msg, tg_user)
+    
+         else:
+             self.tg_client.send_message(
+                 chat_id=msg.chat.id,
+                 text=f"Неизвестная команда {msg.text}"
+             )
+
+    def handle(self, *args, **options):
+        offset = 0
+        while True:
+            res = self.tg_client.get_updates(offset=offset)
+            for item in res.result:
+                offset = item.update_id + 1
+                self.tg_client.send_message(
+                    chat_id=item.message.chat.id,
+                    text=item.message.text
+                )
+
